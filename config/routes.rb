4cb4993ef5c4d83415d0277b7bc0856914826eb7
borourke <borourke@crowdflower.com>
2016-07-21 12:03:26 -0700

    add json controller for returning index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'templates#home'
 
   get '/templates', to: 'templates#home', as: :templates
+  get '/templates/index', to: 'templates#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+template = Template.create(job_id: 847455, title: "Super Cool Job Template", author: "borourke@crowdflower.com", summary: "This is a super cool job template for Brian Hazell", description: "This is a super cool job template.  It does super cool things.  Things so cool I'd be surprised if you have ever heard of them before.  You probably haven't, so let's just assume you're not as cool as this job template.", rating: 4)
+
+Review.create(template_id: template.id, review: "Wow this template is so cool.  It's probalby the coolest template I've ever seen.  If there was a cooler template it would be an already completed job with 100% accuracy.", reviewer: "bbeck@crowdflower.com", rating: 5)
+
+Review.create(template_id: template.id, review: "What's up with this job?  It's pretty cool", reviewer: "sydney@crowdflower.com", rating: 4)
+
+Review.create(template_id: template.id, review: "Is it just me or does Brian smell funny?", reviewer: "romeo@crowdflower.com", rating: 3)
+
+Tag.create(template_id: template.id, tag: "Text Annotation")
+Tag.create(template_id: template.id, tag: "NLP")
+Tag.create(template_id: template.id, tag: "Text Highlighting")
+
+template2 = Template.create(job_id: 847410, title: "Whatevs Kinda Template", author: "kirsten@crowdflower.com", summary: "This template is kinda whatevs if you ask me.", description: "This is a super cool job template.  It does super cool things.  Things so cool I'd be surprised if you have ever heard of them before.  You probably haven't, so let's just assume you're not as cool as this job template.", rating: 3)
+
+
