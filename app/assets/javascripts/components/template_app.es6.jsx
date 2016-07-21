fa562377b6b5d5b4b971b63b8d0b2b1c5add8ea5
Ben Beckerman <benjamin.beckerman@crowdflower.com>
2016-07-21 11:35:17 -0700

    added bootstrap, added erb file

--- app/assets/javascripts/components/template_app.es6.jsx
+++ app/assets/javascripts/components/template_app.es6.jsx
@@ -1,6 +1,15 @@
 class TemplateApp extends React.Component {
   render () {
-    return <div />;
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-2'></div>
+          <div className='col-xs-8'>
+            <h2>Pokedex</h2>
+          </div>
+          <div className='col-xs-2'></div>
+        </div>
+      </div>
+    );
   }
 }
-
