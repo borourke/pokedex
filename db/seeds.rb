f1388e0cfa00925605c42fde7436c025cfd282d1
borourke <borourke@crowdflower.com>
2016-07-22 13:25:37 -0700

    seeds to use app directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_location = "/Users/benjaminbeckerman/Desktop/pokedex_seeds.csv"
+csv_location = Rails.root.join('db', 'pokedex_seeds.csv')
 possible_authors = %w(borourke@crowdflower.com bbeck10@crowdflower.com sydney@crowdflower.com romeo@crowdflower.com blake@crowdflower.com kirsten@crowdflower.com brian@crowdflower.com)
 CSV.foreach(csv_location, headers: true) do |row|
   template = Template.create(job_id: row["SandboxJobID"], title: row["Title"], author: possible_authors[rand(possible_authors.length)], summary: row["Summary"], description: row["Description"], rating: row["Rating"].to_i, image_url: row["Image"])
